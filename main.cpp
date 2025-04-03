9188488a9d79a36db2d7a22d322982417eea1fbc
Albert Castillo <Albert.castillom@gmail.com>
2025-04-03 13:48:46 -0700

    Adding files

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool is_power_of_two(int num);
+bool is_power_of_two_noLoop(int num);
+bool is_prime(int num, int numSqrt);
+
+struct Fraction
+{
+    int numerator;
+    int denominator;
+};
+
+void printFraction(const Fraction & fraction)
+{
+    cout << fraction.numerator << "/" << fraction.denominator << endl;
+}
+
+int getGCD(int a, int b)
+{
+    int remainder = a % b;
+    if (remainder == 0)
+    {
+        return b;
+    }
+    else
+    {
+        return getGCD(b, remainder);
+    }
+}
+
+int main()
+{
+
+    const int num = 5; //num for finding power of 2 and if its a prime
+    int numSqrt = round(sqrt(num)); // sqrt of num to find if its a prime
+    bool primeResult = is_prime(num, numSqrt);
+    bool result = is_power_of_two(num);
+    bool second_result = is_power_of_two_noLoop(num);
+    cout << boolalpha << result << endl;
+    cout << boolalpha <<"no loop result: "<< second_result << endl;
+    (primeResult) ? cout << "pass\n" : cout << "fail\n";
+
+    Fraction fraction;
+    fraction.numerator = 14;
+    fraction.denominator = 5;
+    int gcd = getGCD(fraction.denominator, fraction.numerator);
+    Fraction fractionReduced;
+    fractionReduced.numerator = fraction.numerator/gcd;
+    fractionReduced.denominator = fraction.denominator/gcd;
+    printFraction(fraction);
+    printFraction(fractionReduced);
+
+    return 0;
+}
+
+bool is_power_of_two(int num)
+{
+    if (num == 0 )
+    {
+        return false;
+    }
+    while (num != 1)
+    {
+        if (num % 2 ==0)
+        {
+            num = num/2;
+        }
+        else if (num % 2 !=0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool is_power_of_two_noLoop(int num)
+{
+    if (num == 0 )
+    {
+        return false;
+    }
+    if ( (num & (num - 1)) == 0) //essentially if a number in its bit format only has one "1" and the rest are "0"s
+        //its a power of two. if you subtract one from the number and use the And operator it will return all 0s if its a power of two
+        //the And symbol compares each bit and returns 0 if they are different
+    // 8 - 1 = 7 -> binary -> 111
+    // 8 -> binary -> 1000
+    // and symbol compares 0 and 1, then 1 and 0 three times. which return 0 overall.
+    {
+        return true;
+    }
+    return false;
+}
+
+bool is_prime (int num, int numSqrt)
+{
+    if (num == 0 || num == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i<numSqrt; i++)
+    {
+        if (num % i == 0 )
+        {
+            return false;
+        }
+    }
+    return true;
+}
